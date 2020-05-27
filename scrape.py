d3873c7987143483a204934fc1172688c75e4aa6
Scottie Enriquez <scottie@enriquez.xxx>
2020-05-27 00:29:34 -0500

    Refactoring base logic into method; adding initial for loop structure

--- scrape.py
+++ scrape.py
@@ -1,17 +1,27 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = "https://247sports.com/Season/2020-Football/CompositeRecruitRankings/?InstitutionGroup=HighSchool"
+year_range = range(2020, 2021)
 
-request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-response = urllib.request.urlopen(request)
-html = response.read()
-soup = BeautifulSoup(html, 'html.parser')
-rankings_page_div = soup.find('div', class_='rankings-page__main')
-list_items = rankings_page_div.find_all(
-    'li', class_='rankings-page__list-item')
-for list_item in list_items:
-    name_link = list_item.find('a', class_='rankings-page__name-link')
-    if(name_link != None):
-        print(name_link['href'].split('-')[-1])
-        print(name_link.text)
+
+def parse_page_of_recruits(url):
+    request = urllib.request.Request(
+        url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(request)
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    rankings_page_div = soup.find('div', class_='rankings-page__main')
+    list_items = rankings_page_div.find_all(
+        'li', class_='rankings-page__list-item')
+    for list_item in list_items:
+        name_link = list_item.find('a', class_='rankings-page__name-link')
+        if(name_link != None):
+            print(name_link['href'].split('-')[-1])
+            print(name_link.text)
+
+
+for year in year_range:
+    for page_index in range(1, 2):
+        url = f'https://247sports.com/Season/{year}-Football/CompositeRecruitRankings/?page={page_index}'
+        print(url)
+        parse_page_of_recruits(url)
