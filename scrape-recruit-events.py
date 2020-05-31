4b296825288af7aa7186e27ca2ee4cac297e4b8f
Callen Trail <callentrail@gmail.com>
2020-05-30 22:50:33 -0500

    add output write to scrape-recruit-events.py

--- scrape-recruit-events.py
+++ scrape-recruit-events.py
@@ -1,12 +1,17 @@
 from http_utility import http_get
 from bs4 import BeautifulSoup
 from datetime import datetime
+import json
+import glob
 
 def event_pages(player_url):
     url = f'{player_url}/TimelineEvents/'
     html = http_get(url)
     base_page = BeautifulSoup(html, 'html.parser')
-    num_event_pages = base_page.find_all('a', class_ = 'pagn_link')[-2].get_text()
+    try:
+        num_event_pages = base_page.find_all('a', class_ = 'pagn_link')[-2].get_text()
+    except:
+        num_event_pages = 1
     return int(num_event_pages)
 
 def get_event_items(url):
@@ -16,7 +21,6 @@ def get_event_items(url):
     list_items = events_page_ul.find_all( 'div', class_ = 'left')
     for list_item in list_items:
         event = {}
-        #name_link = list_item.
         if list_item is not None:
             parse_player_from_url = url.split('/')[-3]
             name_id_split = parse_player_from_url.split('-')
@@ -30,17 +34,27 @@ def get_event_items(url):
             event = {
                 '247_id': player_id,
                 'player_name': player_name,
-                'event_date': event_type,
-                'event_type': date_convert.strftime('%Y/%m/%d'),
+                'event_type': event_type,
+                'event_date': date_convert.strftime('%Y-%m-%d'),
                 'event_description': event_description
             }
             event_list.append(event)
 
 
-player_url = 'https://247sports.com/Player/Bryan-Bresee-46038819/'
-event_list = []
-recruit_pages = event_pages(player_url)
-for pagen in range(1, recruit_pages + 1):
-    url = url = f'https://247sports.com/Player/Bryan-Bresee-46038819/TimelineEvents/?Page={pagen}'
-    get_event_items(url)   
-print(event_list)
+jsonfiles = []
+path = './recruit-lists/test-list.json'
+for file in glob.glob(path):
+    with open(file,'r') as recruit_list:
+        for cnt, line in enumerate(recruit_list):
+            recruit = json.loads(line)
+            player_url = recruit['247_url']
+            recruit_pages = event_pages(player_url)
+            event_list = []
+            for pagen in range(1, recruit_pages + 1):
+                url = f'{player_url}/TimelineEvents/?Page={pagen}'
+                get_event_items(url)   
+            print(event_list)
+            file_name = f'./recruit-lists/recruit-lists-events.json'
+            with open(file_name, 'w') as output_file:
+                json.dump(event_list,output_file)
+            print(f'Wrote {file} recruits to {file_name}')
