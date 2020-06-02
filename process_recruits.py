6240cc18104c60703f92d43f60f93a0b8e8aa02a
Scottie Enriquez <scottie@enriquez.xxx>
2020-06-02 10:38:29 -0500

    Adding error checking for recruits with no ranking history

--- process_recruits.py
+++ process_recruits.py
@@ -24,7 +24,11 @@ with open(recruit_list_file_path, 'r') as recruit_list_file:
     for count, line in enumerate(recruit_list_file):
         recruit = json.loads(line)
         ranking_history_url = get_ranking_history_url(recruit['247_url'], recruit['full_name'])
-        ranking_history = get_recruiting_ranking_history(recruit['247_id'], ranking_history_url, recruit['full_name'])
+        if ranking_history_url is not None:
+            ranking_history = get_recruiting_ranking_history(recruit['247_id'], ranking_history_url, recruit['full_name'])
+        else:
+            print(emoji.emojize(f':warning: No ranking history URL found for {recruit["full_name"]}', use_aliases=True))
+            ranking_history = []
         timeline_events = get_recruiting_timeline(recruit['247_id'], recruit['247_url'], recruit['full_name'])
         ranking_history_file_name = f'{recruit_ranking_history_output_directory_base}/{year}/{recruit["247_id"]}.json'
         with open(ranking_history_file_name, 'a') as ranking_history_file:
