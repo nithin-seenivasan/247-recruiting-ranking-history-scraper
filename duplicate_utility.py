561097b68801ddac6565b7fae78f069072aebaa9
Scottie Enriquez <scottie@enriquez.xxx>
2020-06-08 18:11:25 -0500

    Refactoring merge utility; adding duplicate utility; updating README

---
+++ duplicate_utility.py
@@ -0,0 +1,36 @@
+import sys
+import json
+import emoji
+
+
+def remove_duplicates(list_file_path, identifying_attribute):
+    exists = {}
+    duplicates = {}
+    duplicates_handled = {}
+    with open(list_file_path, 'r') as list_file:
+        for index, line in enumerate(list_file):
+            data = json.loads(line)
+            if exists.get(data[identifying_attribute]):
+                print(emoji.emojize(f':thumbsdown: Duplicate data found on line {index}: {data[identifying_attribute]}', use_aliases=True))
+                if data[identifying_attribute] in duplicates:
+                    duplicates[data[identifying_attribute]].push(index)
+                else:
+                    duplicates[data[identifying_attribute]] = [index]
+            else:
+                exists[data[identifying_attribute]] = True
+    if len(duplicates) > 0:
+        print(emoji.emojize(f':thumbsdown: {len(duplicates)} duplicates found', use_aliases=True))
+        with open(list_file_path, 'r') as list_file:
+            lines = list_file.readlines()
+        with open(list_file_path, 'w') as list_file:
+            for line in lines:
+                data = json.loads(line)
+                if data[identifying_attribute] not in duplicates or data[identifying_attribute] not in duplicates_handled:
+                    if data[identifying_attribute] in duplicates:
+                        duplicates_handled[data[identifying_attribute]] = True
+                    list_file.write(line)
+            print(emoji.emojize(f':thumbsup: Duplicates removed', use_aliases=True))
+
+
+list_file_path = sys.argv[1]
+remove_duplicates(list_file_path, identifying_attribute='247_id')
--- merge_utility.py
+++ merge_utility.py
@@ -1,28 +1,18 @@
+import sys
 import os
 
-def merge_timeline_history():
-    for root, dirs, files in os.walk('./recruit-timeline-histories/', onerror=None):
-        for file in files:
-            path = os.path.join(root, file)
-            f = open(path)
-            f2 = open("merged-timelines.json", "a")
-            for line in f.readlines():
-                f2.write(line)
 
-def merge_ranking_history():
-    for root, dirs, files in os.walk('./recruit-ranking-histories/', onerror=None):
+def merge_list(list_directory_path, output_file_path):
+    for root, directories, files in os.walk(list_directory_path, onerror=None):
         for file in files:
             path = os.path.join(root, file)
-            f = open(path)
-            f2 = open("merged-rankings.json", "a")
-            for line in f.readlines():
-                f2.write(line)
+            file_to_merge = open(path)
+            output_file = open(output_file_path, 'a')
+            for line in file_to_merge.readlines():
+                output_file.write(line)
+                output_file.write(line)
 
-def merge_recruit_list():
-    for root, dirs, files in os.walk('./recruit-lists/', onerror=None):
-        for file in files:
-            path = os.path.join(root, file)
-            f = open(path)
-            f2 = open("merged-recruits.json", "a")
-            for line in f.readlines():
-                f2.write(line)
+
+list_directory_path = sys.argv[1]
+output_file_path = sys.argv[2]
+merge_list(list_directory_path, output_file_path)
